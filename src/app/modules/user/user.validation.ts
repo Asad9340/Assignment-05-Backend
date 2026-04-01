2c71940fe96f4c0bf9ce1cb2f64f892ffde952e8
Asad9340 <asadulimran1999@gmail.com>
2026-04-02 01:29:26 +0600

    image upload problem fix

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const updateUserValidationSchema = z.object({
-  name: z.string().min(2).max(100).optional(),
-  image: z.string().url().optional(),
+  body: z.object({
+    name: z.string().min(2).max(100).optional(),
+    image: z.string().url().optional(),
+  }),
 });
